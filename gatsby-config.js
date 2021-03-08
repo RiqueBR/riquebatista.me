694af3ea8f5b74ee1f2e20964d42ec0521395ef6
Henrique Batista <henrique.batista@ed.ac.uk>
2021-03-08 22:00:41 +0000

    trying to fix package build errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,17 +27,6 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-      },
-    },
-    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'projects',
